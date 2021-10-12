bdfd51f200f35813eb5bfb6bc29d53ddcc6a3172
Thien My <myntuit@gmail.com>
2021-10-12 17:55:24 +0900

    add AddEditTask fragment

---
+++ app/src/main/java/com/mynt/todo/presentation/addedittask/AddEditTaskFragment.kt
@@ -0,0 +1,20 @@
+package com.mynt.todo.presentation.addedittask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mynt.todo.R
+
+class AddEditTaskFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.addtask_frag, container, false)
+    }
+
+}
